7f74c899e5fe76900ac6b9ab6d0c53239fe2ac76
aryan1403 <aaryan14032006@gmail.com>
2024-07-25 15:18:58 +0530

    navigation

--- demo/src/components/app.tsx
+++ demo/src/components/app.tsx
@@ -12,11 +12,57 @@ import Context = require("ojs/ojcontext");
 import { Footer } from "./footer";
 import { Header } from "./header";
 import { Content } from "./content/index";
+import CoreRouter = require("ojs/ojcorerouter");
+import UrlParamAdapter = require("ojs/ojurlparamadapter");
+import {useState} from "preact/hooks";
 
 type Props = Readonly<{
   appName?: string;
   userLogin?: string;
 }>;
+
+type Route = {
+  path: string;
+  detail? : Object;
+  redirect? : String;
+}
+
+function toRecord<
+  T extends { [K in keyof T]: string | number | symbol }, // added constraint
+  K extends keyof T
+>(array: T[], selector: K): Record<T[K], T> {
+  return array.reduce((acc, item) => (acc[item[selector]] = item, acc), {} as Record<T[K], T>)
+}
+
+const routeArray: Array<Route> = [
+  { path: "", redirect: "home"},
+  {
+    path: "home",
+    detail: {
+      label: "Home Page",
+    }
+  },
+  {
+    path: "about",
+    detail: {
+      label: "About Page",
+    }
+  },
+  {
+    path: "Contact",
+    detail: {
+      label: "Contact Page",
+    }
+  },
+];
+
+const router = new CoreRouter<CoreRouter.DetailedRouteConfig>(Object(routeArray), {
+  urlAdapter: new UrlParamAdapter(),
+});
+
+const pageChangeHandler = (value: string) => {
+  router.go({path: value})
+}
 
 export const App = registerCustomElement(
   "app-root",
@@ -24,12 +70,28 @@ export const App = registerCustomElement(
     useEffect(() => {
       Context.getPageContext().getBusyContext().applicationBootstrapComplete();
     }, []);
+
+    const [routePath, setroutePath] = useState<string>("");
+
+    useEffect(() => {
+      Context.getPageContext().getBusyContext().applicationBootstrapComplete();
+      router.currentState.subscribe(routerUpdated);
+      router.sync();
+    }, []);
+
+    const routerUpdated = (actionable: CoreRouter.ActionableState<CoreRouter.DetailedRouteConfig>): void => {
+      const newPath = actionable.state?.path;
+      setroutePath(newPath);
+    }
     
     return (
       <div id="appContainer" class="oj-web-applayout-page">
         <Header
           appName={appName} 
           userLogin={userLogin} 
+          page={routePath}
+          onPageChanged={pageChangeHandler}
+          routes={routeArray}
         />
         <Content />
         <Footer />
--- demo/src/components/header.tsx
+++ demo/src/components/header.tsx
@@ -11,18 +11,38 @@ import * as ResponsiveUtils from "ojs/ojresponsiveutils";
 import "ojs/ojtoolbar";
 import "ojs/ojmenu";
 import "ojs/ojbutton";
+import ArrayDataProvider = require("ojs/ojarraydataprovider");
+import { ojNavigationList } from "ojs/ojnavigationlist";
 
 type Props = Readonly<{
   appName: string,
-  userLogin: string
+  userLogin: string,
+  page?: string,
+  routes: Array<Object>,
+  onPageChanged: (value: string) => void;
 }>;
 
-export function Header({ appName, userLogin }: Props) {
+type Route = {
+  path: string;
+  detail: object;
+}
+
+export function Header({ appName, userLogin, page, onPageChanged, routes }: Props) {
+  const [currentPage, setcurrentPage] = useState<string>(page ? page : "home");
+  const routesDP = new ArrayDataProvider(routes.slice(1), { keyAttributes: "path" });
+
+
+  const onPageChangedHandler = (event: ojNavigationList.selectionChanged<Route["path"], Route>) => {
+    if(event.detail.updatedFrom === "internal") {
+      onPageChanged(event.detail.value);
+    }
+  }
   const mediaQueryRef = useRef<MediaQueryList>(window.matchMedia(ResponsiveUtils.getFrameworkQuery("sm-only")!));
   
   const [isSmallWidth, setIsSmallWidth] = useState(mediaQueryRef.current.matches);
 
   useEffect(() => {
+
     mediaQueryRef.current.addEventListener("change", handleMediaQueryChange);
     return (() => mediaQueryRef.current.removeEventListener("change", handleMediaQueryChange));
   }, [mediaQueryRef]);
@@ -39,34 +59,38 @@ export function Header({ appName, userLogin }: Props) {
     return (isSmallWidth ? "oj-icon demo-appheader-avatar" : "oj-component-icon oj-button-menu-dropdown-icon");
   }
 
+  const navList = (item: ojNavigationList.ItemContext<string, Route>) => {
+    console.log(item);
+    
+    return (
+      <li id={item.data.path}>
+        <a href="#">
+          {console.log(item.data.detail.label)}
+          {item.data.detail.label}
+          {getDisplayType() == "all" ? item.data.detail.label : ""}
+        </a>
+      </li>
+    )
+  }
+
   return (
     <header role="banner" class="oj-web-applayout-header">
       <div class="oj-web-applayout-max-width oj-flex-bar oj-sm-align-items-center">
-        <div class="oj-flex-bar-middle oj-sm-align-items-baseline">
-          <span
-            role="img"
-            class="oj-icon demo-oracle-icon"
-            title="Oracle Logo"
-            alt="Oracle Logo"></span>
-          <h1
-            class="oj-sm-only-hide oj-web-applayout-header-title"
-            title="Application Name">
-            {appName}
-          </h1>
-        </div>
         <div class="oj-flex-bar-end">
-        <oj-toolbar>
-          <oj-menu-button id="userMenu" display={getDisplayType()} chroming="borderless">
-            <span>{userLogin}</span>
-            <span slot="endIcon" class={getEndIconClass()}></span>
-            <oj-menu id="menu1" slot="menu">
-              <oj-option id="pref" value="pref">Preferences</oj-option>
-              <oj-option id="help" value="help">Help</oj-option>
-              <oj-option id="about" value="about">About</oj-option>
-              <oj-option id="out" value="out">Sign Out</oj-option>
-            </oj-menu>
-          </oj-menu-button>
-        </oj-toolbar>
+        <div role="navigation" class="oj-web-applayout-max-width oj-web-applayout-navbar">
+            <oj-navigation-list
+              selection={page}
+              edge="top"
+              id="navlist1"
+              aria-label="navigation"
+              onselectionChanged={onPageChangedHandler}
+              drillMode="none"
+              data={routesDP}
+            >
+              <template slot="itemTemplate" render={navList}/>
+
+            </oj-navigation-list>
+          </div>
         </div>
       </div>
     </header>
